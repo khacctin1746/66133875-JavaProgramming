3c1f3eab0864d54d56bea1a539f4b5dc2666eee3
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-01-30 08:01:54 +0700

    Add files via upload

---
+++ BAITHUCHANH_1/tinhTienLuong/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        TienLuong tienLuong = new TienLuong();
+
+        tienLuong.nhapThongTin();
+        tienLuong.tinhTienLuong();
+        tienLuong.xuatTienLuong();
+    }
+}
