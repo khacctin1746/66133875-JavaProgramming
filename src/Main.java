bff3eb083e5e12a6f1bd3b0136c6209dd6d9af90
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-02-10 18:21:40 +0700

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Xin chào thầy Thọ!");
-        System.out.println("Test git push: " + 36);
-    }
-}
