a2b8c7c692cafc010219ebf378b2dc1353d25fbe
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-02-10 18:21:54 +0700

    Delete BAITHUCHANH_1 directory

--- BAITHUCHANH_1/tinhTienLuong/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        TienLuong tienLuong = new TienLuong();
-
-        tienLuong.nhapThongTin();
-        tienLuong.tinhTienLuong();
-        tienLuong.xuatTienLuong();
-    }
-}
