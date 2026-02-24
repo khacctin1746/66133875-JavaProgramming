050d982e66335212e6469160ad0e4c749f743ff4
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-02-24 22:22:09 +0700

    init commit

---
+++ TinhTienLuong/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int luong = scanner.nextInt();
+        System.out.println(luong);
+    }
+}
