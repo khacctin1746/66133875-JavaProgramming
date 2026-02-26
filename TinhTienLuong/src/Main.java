48b9c3588f904654d150672c421d9adec52df427
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-02-26 16:09:20 +0700

    final code

--- TinhTienLuong/src/Main.java
+++ TinhTienLuong/src/Main.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Vui long nhap luong theo gio: ");
         int luong = scanner.nextInt();
-        System.out.println(luong);
+        System.out.print("Vui long nhap so gio lam: ");
+        int gio = scanner.nextInt();
+
+        System.out.println("Tong luong cua nguoi nay la: " + luong*gio);
     }
 }
