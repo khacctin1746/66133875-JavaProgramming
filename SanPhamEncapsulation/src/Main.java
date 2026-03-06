d3bc1e1bb3d95e83c5e7a3d924f86c9ca5c772eb
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-03-06 09:43:00 +0700

    update

--- SanPhamEncapsulation/src/Main.java
+++ SanPhamEncapsulation/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+        // test case 1:
+        SanPham sp1 = new SanPham();
+        sp1.setMaSP("UU1QK");
+        sp1.setTenSP("Keo Mut");
+        sp1.setGia(10000);
+        sp1.setSoLuong(2);
+        sp1.tinhThanhTien();
+        sp1.hienThiThongTin();
+
+        // test case 2:
+        SanPham sp2 = new SanPham();
+        sp2.hienThiThongTin();
+
+        //test case 3:
+        SanPham sp3 = new SanPham();
+        sp3.setMaSP("");
+        sp3.setTenSP("");
+        sp3.setGia(-1);
+        sp3.setSoLuong(-1);
     }
+
 }
--- SanPhamEncapsulation/src/SanPham.java
+++ SanPhamEncapsulation/src/SanPham.java
@@ -1,24 +1,28 @@
 public class SanPham {
-    private int maSP;
+    private String maSP;
     private String tenSP;
     private int gia;
     private int soLuong;
+    private double thanhTien;
 
     //constructor default
     public SanPham() {
-        this.maSP = 0;
+        this.maSP = "empty";
         this.tenSP = "empty";
         this.gia = 0;
         this.soLuong = 0;
     }
 
     //constructor
-    public SanPham(int maSP, String tenSP, int gia, int soLuong) {
+    public SanPham(String maSP, String tenSP, int gia, int soLuong) {
         this.maSP = maSP;
+        this.tenSP = tenSP;
+        this.gia = gia;
+        this.soLuong = soLuong;
     }
 
     // Getter
-    public int getMaSP() {
+    public String getMaSP() {
         return maSP;
     }
 
@@ -41,5 +45,37 @@ public class SanPham {
         } else {
             this.gia = 0;
         }
+    }
+
+    public void setMaSP(String maSP) {
+        if(maSP != null && !maSP.trim().isEmpty()) {
+            this.maSP = maSP;
+        } else {
+            System.out.println("Ma san pham khong hop le");
+        }
+    }
+
+    public void setTenSP(String tenSP) {
+        if(tenSP != null && !tenSP.trim().isEmpty()) {
+            this.tenSP = tenSP;
+        } else {
+            System.out.println("Ten pham khong hop le");
+        }
+    }
+
+    public void setSoLuong(int soLuong) {
+        if(!(soLuong <= 0)) {
+            this.soLuong = soLuong;
+        } else {
+            System.out.println("So luong khong hop le, so luong phai >= 0");
+        }
+    }
+
+    public void tinhThanhTien() {
+        this.thanhTien = gia*soLuong;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Thong Tin HD - Ma: " + this.maSP + " | Ten SP: " + this.tenSP + " | Gia: " + gia + " | So luong: " + soLuong + " | Thanh tien: " + thanhTien);
     }
 }
