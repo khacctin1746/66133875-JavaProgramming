404d563009f086bcb506f4d8054c22638a2cca36
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-04-15 22:02:42 +0700

    update

---
+++ VD1_Chuong5/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class ReadLine
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        System.out.println("Nhap chuoi.");
+        System.out.println("Nhap 'stop' ket thuc chuong trinh.");
+
+        do
+        {
+            str = br.readLine();
+            System.out.println(str);
+        } while(!str.equals("stop"));
+    }
+}
