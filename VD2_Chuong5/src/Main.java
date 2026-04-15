1432feb0df294fcf110d3a6ee4391be5ec8bb20a
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-04-15 22:05:04 +0700

    update

---
+++ VD2_Chuong5/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        int i = 10;
+        double d = 123.67;
+        double r = i + d;
+
+        pw.println("Using a PrintWriter.");
+        pw.println(i);
+        pw.println(d);
+        pw.println(i + " + " + d + " = " + r);
+    }
+}
