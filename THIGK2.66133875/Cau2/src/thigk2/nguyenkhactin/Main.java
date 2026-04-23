8f0b127073d8d8ea525d4db828015f84b7c7d4c9
khacctin <108194687+khacctin1746@users.noreply.github.com>
2026-04-23 07:56:22 +0700

    update constructor & getter setter cau 2

---
+++ THIGK2.66133875/Cau2/src/thigk2/nguyenkhactin/Main.java
@@ -0,0 +1,7 @@
+package thigk2.nguyenkhactin;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ THIGK2.66133875/Cau2/src/thigk2/nguyenkhactin/packSV/SinhVien.java
@@ -0,0 +1,56 @@
+package thigk2.nguyenkhactin.packSV;
+
+public class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private int namSinh;
+    private String gioiTinh;
+
+    // khai bao constructor co tham so
+    public SinhVien() {
+        this.maSV = "";
+        this.hoTen = "";
+        this.namSinh = 0;
+        this.gioiTinh = "";
+    }
+
+    // khai bao constructor khong co tham so
+    public SinhVien(String maSV, String hoTen, int namSinh, String gioiTinh) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public int getNamSinh() {
+        return namSinh;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public void setNamSinh(int namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+}
